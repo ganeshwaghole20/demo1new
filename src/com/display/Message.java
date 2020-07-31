ce67f4c2bd146474315cf1808a0a891a9f2bc7a0
GANESH-NETRA-SWINAL <GANESH-NETRA-SWINAL@GANESH-NETRA-SW>
2020-07-31 18:50:16 +0530

    first commit to demo1

---
+++ src/com/display/Message.java
@@ -0,0 +1,12 @@
+package com.display;
+
+public class Message {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello welcome to git hub system");
+
+	}
+
+}
